db98f16783f2138a47192c56d9b9bb056c3e9a68
wolfogre <wolfogre@qq.com>
2015-12-03 15:48:46 +0800

    Added Week2

---
+++ Homework/Week2/Solve.m
@@ -0,0 +1,36 @@
+function y = Solve(Fun)
+%Input    - Fun is the function 
+%Output - y is the zero
+a = -1;
+b = 1;
+while Fun(a) * Fun(b) >= 0 
+    if -a>=b
+        a = a * 2;
+    else
+        b = a * 2;
+    end
+end
+
+while b - a > 1
+    mid = (a + b) / 2;
+    if Fun(a) * Fun(mid) <= 0
+        b = mid;
+    else
+        a = mid;
+    end
+end
+
+p0 = a;
+p1 = b;
+for k=1:10000	
+	p2=p1-Fun(p1)*(p1-p0)/(Fun(p1)-Fun(p0));	
+	err=abs(p2-p1);
+	p0=p1;
+	p1=p2;
+	if (err<0.0000001)
+        break;
+    end
+end
+y = p0;
+end
+
