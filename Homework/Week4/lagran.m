7af8946c9518edd0ab20d8e1a688a87dc14468ce
Jason Song <wolfogre@outlook.com>
2015-12-18 12:28:11 +0800

    Week4

---
+++ Homework/Week4/lagran.m
@@ -0,0 +1,60 @@
+function [C,L,y0]=lagran(X,Y,x0)
+
+%Input    - X is a vector that contains a list of abscissas
+%            - Y is a vector that contains a list of ordinates
+%Output - C is a matrix that contains the coefficents of
+%              the Lagrange interpolatory polynomial
+%            - L is a matrix that contains the Lagrange
+%              coefficient polynomials
+
+%  NUMERICAL METHODS: Matlab Programs
+% (c) 2004 by John H. Mathews and Kurtis D. Fink
+%  Complementary Software to accompany the textbook:
+%  NUMERICAL METHODS: Using Matlab, Fourth Edition
+%  ISBN: 0-13-065248-2
+%  Prentice-Hall Pub. Inc.
+%  One Lake Street
+%  Upper Saddle River, NJ 07458
+
+w=length(X);
+n=w-1;
+L=zeros(w,w);
+
+%Form the Lagrange coefficient polynomials
+
+for k=1:n+1
+   V=1;
+   for j=1:n+1
+      if k~=j
+         V=conv(V,poly(X(j)))/(X(k)-X(j));
+      end
+   end
+   L(k,:)=V;
+end
+
+%Determine the coefficients of the Lagrange interpolator
+%polynomial
+
+C=Y*L;
+function y =fun(x)
+y = 0;
+for i = 1 : length(C)
+    y = y + C(i) * power(x, length(C) - i);
+end
+end
+
+y0 = fun(x0);
+x = [0:pi/100: 2 * pi];
+y = fun(x);
+
+
+plot(x,y,'r');
+hold on;
+y = sin(x);
+plot(x,y);
+hold on;
+plot(X,Y,'ro');
+hold on;
+plot(x0,y0,'kv');
+end
+   
